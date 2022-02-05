476e052c0d699675af0e5eb505fc128cc32b8922
Dumebii <98858882+Dumebii@users.noreply.github.com>
2022-02-05 01:42:22 +0100

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -28,8 +28,8 @@ with open("./SimpleStorage.sol", "r") as file:
 
     w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
     chain_id = 1337
-    my_address = "0xa532276A0190698195E5079790Ce3A686f64FdD5"
-    private_key = "0x3f26f1b066d46aa1102514a851059b3913d9f10587e2014d01eabfa76e904197"
+    my_address = "0x66A19bEc114A282ca7805B840046a1b326101Cb9"
+    private_key = "0x9a8e001e8b5fd4ff7f61537540e934ae8fa7f313df23e5afa0f2f1117c46fbcd"
 
     SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
@@ -42,3 +42,15 @@ with open("./SimpleStorage.sol", "r") as file:
     signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
     tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
     tx_reciept = w3.eth.wait_for_transaction_receipt(tx_hash)
+    simple_storage = w3.eth.contract(address=tx_reciept.contractAddress, abi=abi)
+    print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+    greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "gasPrice": w3.eth.gas_price, "from": my_address, "nonce": nonce + 1})
+
+    signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+    )
+    tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+    print("Updating stored Value...")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+    print(simple_storage.functions.retrieve().call())
